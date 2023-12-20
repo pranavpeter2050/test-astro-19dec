339ad5cb43782bdf96b4dc809bd1cb66f2dcada4
pranavpeter2050 <97501797+pranavpeter2050@users.noreply.github.com>
2023-12-20 12:28:39 +0530

    working with Markdown & Content Collections
    -  added dummy blog post content from tutorial demo project. https://github.com/jamesqquick/astro-course-demo/tree/main/src/content/posts
    - edited markdown related config file (typescript by default)

---
+++ src/content/config.ts
@@ -0,0 +1,15 @@
+import {z, defineCollection} from "astro:content";
+import {format} from "date-fns";
+
+const postsCollection = defineCollection({
+    schema: z.object({
+            author: z.string(),
+            date: z.string(),
+            image: z.string(),
+            title: z.string(),
+        }),
+})
+
+export const collections = {
+    posts: postsCollection,
+}
